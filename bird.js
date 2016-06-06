771582fef8876565a7aecc413be49bacfdced86c
Hector Romo <info@hectorromo.com>
2016-06-06 22:07:36 +0200

    first commit

---
+++ bird.js
@@ -0,0 +1,23 @@
+function Bird() {
+  
+  this.eye = ellipse(20, 20, 20, 20);
+  
+  this.beak = triangle(30, 72, 20, 20, 20, 20);
+  
+  this.flap = function() {
+    
+  }
+  
+  this.collide = function() {
+    
+  }
+  
+  this.move = function() {
+    
+  }
+  
+  this.scream = function() {
+    
+  }
+  
+}
